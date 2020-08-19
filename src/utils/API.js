1ffb4a473ea45661de09405cecc6c9e458176a60
MatiasDaldi44 <62301967+MatiasDaldi44@users.noreply.github.com>
2020-08-19 15:03:09 -0400

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default {
     getLeaguesFrom: function (country) {
-        return axios.get("https://www.thesportsdb.com/api/v1/json/4013017/search_all_leagues.php?c=" + country + "&s=Soccer")
+        return axios.get("https://www.thesportsdb.com/api/v1/json/1/search_all_leagues.php?c=" + country + "&s=Soccer")
     },
 
     getAllCountries: function () {
-        return axios.get("https://www.thesportsdb.com/api/v1/json/4013017/all_countries.php")
+        return axios.get("https://www.thesportsdb.com/api/v1/json/1/all_countries.php")
     }
 }
