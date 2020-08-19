37beb6bcf8117b641af22368a2ff5ed05e8f332b
MD24 <matiasdaldi@gmail.com>
2020-08-19 14:58:11 -0400

    finished basic function of the allLeagues page

--- src/pages/AllLeagues.js
+++ src/pages/AllLeagues.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import API from "../utils/API"
+import TopLeagueDropdown from '../components/TopLeagueDropdown';
+import RowDropdown from '../components/RowDropdown';
 
 const AllLeagues = () => {
-    console.log(API.getLeaguesFrom("England"))
+    const [countryList, setCountryList] = useState([])
+    const topLeagues = [
+        {
+            id: 1,
+            country: "England"
+        },
+        {
+            id: 2,
+            country: "France"
+        },
+        {
+            id: 3,
+            country: "Germany"
+        },
+        {
+            id: 4,
+            country: "Italy"
+        },
+        {
+            id: 5,
+            country: "Spain"
+        }
+    ]
+
+    useEffect(() => {
+        API.getAllCountries()
+            .then(res =>
+                setCountryList(res.data.countries))
+    }, [])
+
+    const handleOnClick = e => {
+        e.preventDefault();
+        console.log("test")
+    }
+
     return (
         <div>
-            <p>Leagues Test</p>
+            <h2>Most Popular</h2>
+            {
+                topLeagues.map(res => {
+                    return <TopLeagueDropdown key={res.id} name={res.country} />
+                })
+            }
+            <br></br>
+            <button onClick={handleOnClick}>Placeholder Console Logging Button</button>
+            <h3>Rest of World</h3>
+            {
+                countryList.sort().map(country => {
+                    return (
+                        <RowDropdown key={country.name_en} name={country.name_en} />
+                    )
+                })
+            }
         </div>
     );
 }
